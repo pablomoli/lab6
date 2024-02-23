1266638947922a5233ff7824f1ff777907ce00d9
Pablo <pablomolinarojas@gmail.com>
2024-02-23 16:01:03 -0500

    search

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,35 +1,33 @@
-#include <stdio.h>
 
-int search(int numbers[], int low, int high, int value) 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int search(int numbers[], int low, int high, int value)
 {
-	while (l <= r) {
-        int m = l + (r - l) / 2;
- 
-        // Check if x is present at mid
-        if (arr[m] == x)
-            return m;
- 
-        // If x greater, ignore left half
-        if (arr[m] < x)
-            l = m + 1;
- 
-        // If x is smaller, ignore right half
-        else
-            r = m - 1;
-    }
- 
-    // If we reach here, then element was not present
-    return -1;
-	
+	if(low>high){
+		return -1;
+	}
+	int mid = (low + high) / 2;
+	if(value == numbers[mid]){
+		return mid;
+	}
+	else if(value < numbers[mid]){
+		return search(numbers, low, mid - 1, value);
+	}
+	else {
+		return search(numbers, mid + 1, high, value);
+	}
 }
 
 void printArray(int numbers[], int sz)
 {
 	int i;
 	printf("Number array : ");
-	for (i = 0;i<sz;++i)
+	for (i = 0; i < sz; ++i)
 	{
-		printf("%d ",numbers[i]);
+		printf("%d ", numbers[i]);
 	}
 	printf("\n");
 }
@@ -37,24 +35,24 @@ void printArray(int numbers[], int sz)
 int main(void)
 {
 	int i, numInputs;
-	char* str;
+	char *str;
 	float average;
 	int value;
 	int index;
-	int* numArray = NULL;
+	int *numArray = NULL;
 	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
+	FILE *inFile = fopen("input.txt", "r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
+		numArray = (int *)malloc(countOfNums * sizeof(int));
 		average = 0;
 		for (i = 0; i < countOfNums; i++)
 		{
-			fscanf(inFile," %d", &value);
+			fscanf(inFile, " %d", &value);
 			numArray[i] = value;
 			average += numArray[i];
 		}
@@ -64,7 +62,7 @@ int main(void)
 		index = search(numArray, 0, countOfNums - 1, value);
 		if (index >= 0)
 		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
+			printf("Item %d exists in the number array at index %d!\n", value, index);
 		}
 		else
 		{
